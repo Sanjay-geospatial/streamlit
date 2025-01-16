9fb5c531aeb698a9bf9e2eabb93afd2dace0deae
Sanjay-geospatial <sanjaymanjappa25@gmail.com>
2025-01-16 13:57:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,12 @@ with st.expander('Data'):
   st.write('#### Raw data')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
   df
+
+  st.write('**x**')
+
+  x = df.drop(columns = 'species')
+  x
+
+  st.write('**y**')
+  y = df['species']
+  y
