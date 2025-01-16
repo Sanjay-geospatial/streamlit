01c10d752580fce44db30aed9e005ff4ae245bf6
Sanjay-geospatial <sanjaymanjappa25@gmail.com>
2025-01-16 13:52:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ st.title('Machine learning app')
 
 st.info('This is a machine learning app')
 
-with st.expander('Data):
+with st.expander('Data'):
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
   df
