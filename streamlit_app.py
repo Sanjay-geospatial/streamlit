d7d9337fc7b4f808166ec13715f964e481a19944
Sanjay-geospatial <sanjaymanjappa25@gmail.com>
2025-04-22 20:04:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ from folium.raster_layers import ImageOverlay
 from branca.colormap import linear
 
 # Load model using huggingface_hub
+@st.cache_resource
 def load_model():
     model_path = hf_hub_download(
         repo_id="SanjayGeospatial/cloud-removal-model",
